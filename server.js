c44796f933bc02c14bfd0b9049c02f73468d9b68
Mohammed_Elamin <melamin100@yahoo.com>
2020-11-05 06:20:10 +0200

    deploy to heroku

--- server.js
+++ server.js
@@ -1,28 +1,51 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000,
+    port = process.env.PORT || 5000,
     mongoose = require('mongoose'),
     Task = require('./api/models/todoListModel'), //created model loading here
     bodyParser = require('body-parser');
 
+
+
+require('dotenv').config()
+
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Tododb', {
-    useMongoClient: true
-});
-
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var routes = require('./api/routes/todoListRoutes'); //importing route
-routes(app); //register the route
 
-app.use(function(req, res) {
-    res.status(404).send({ url: req.originalUrl + ' not found' })
+app.listen(process.env.PORT || 5000, () => {
+    mongoose.set('useFindAndModify', false);
+    mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
 });
 
-app.listen(port);
 
 
-console.log('todo list RESTful API server started on: ' + port);
+
+
+
+
+
+
+const db = mongoose.connection;
+
+
+db.on('error', (error) => {
+    console.log(error)
+});
+
+
+// if we have connection opened then require route file
+db.on('open', () => {
+    var routes = require('./api/routes/todoListRoutes'); //importing route
+    routes(app); //register the route
+
+    app.use(function(req, res) {
+        res.status(404).send({ url: req.originalUrl + ' not found' })
+    });
+    console.log('todo list RESTful API server started on: ' + port);
+
+
+    console.log(`server start on port ---> ${process.env.PORT}`);
+});
